2e01bb7e83665a4195f66838ded43f8babf701a7
Lingxue Li <ll2871@columbia.edu>
2020-11-29 16:04:30 -0700

    pseudocode

---
+++ localusers/add-local-user.sh
@@ -0,0 +1,19 @@
+# make sure the script is being executed with superuser privileges
+
+# get the username
+
+# get the full name
+
+# get the password
+
+# create the user with the password 
+
+# check to see if the useradd command succeed
+
+# set the password
+
+# check to see if the passwd command succeed
+
+# force password change on first login
+
+# display the username, password and host where the user was created
