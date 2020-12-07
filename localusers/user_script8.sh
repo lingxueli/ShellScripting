5114c39966192f78bec1767cb3df08627191f5d9
Lingxue Li <ll2871@columbia.edu>
2020-12-06 22:06:59 -0700

    shell scripting: advanced standard input, standard output, and standard error

--- localusers/user_script8.sh
+++ localusers/user_script8.sh
@@ -22,3 +22,51 @@ echo "${RANDOM} ${RANDOM}" >> ${FILE}
 echo
 echo "Contents of ${FILE}:"
 cat ${FILE}
+
+# redirect STDIN to a program using FD 0
+read LINE 0< ${FILE}
+echo
+echo "LINE contains: ${LINE}"
+
+# redirect STDOUT to a file using FD 1, overwriting the file
+head -n3 /etc/passwd 1> ${FILE}
+echo
+echo "Contents of ${FILE}:"
+cat ${FILE}
+
+# redirect STDERR to a file using FD 2
+ERR_FILE="/tmp/data.err"
+head -n3 /etc/passwd /fakefile 2> ${ERR_FILE}
+
+# redirect STDOUT and STERR to a file
+head -n3 /etc/passwd /fakefile &> ${FILE}
+# or: head -n3 /etc/passwd /fakefile > ${FILE} 2>&1
+echo
+echo "Contents of ${FILE}:"
+cat ${FILE}
+
+# redirect STDOUT and STDERR through a pipe
+echo
+head -n3 /etc/passwd /fakefile |& cat -n
+# or: head -n3 /etc/passwd /fakefile 2>&1 | cat -n
+
+# send STDOUT to STDERR
+echo "This is STDERR!" >&2
+
+# discard STDOUT
+echo
+echo "Discarding STDOUT:"
+head -n3 /etc/passwd /fakefile > /dev/null
+
+# discard STDERR
+echo
+echo "Discarding STDERR:"
+head -n3 /etc/passwd /fakefile 2> /dev/null
+
+# discard STDOUT and STDERR
+echo
+echo "Discarding STDOUT and STDERR"
+head -n3 /etc/passwd /fakefile &> /dev/null
+
+# clean up
+rm ${FILE} ${ERR_FILE} &> /dev/null
