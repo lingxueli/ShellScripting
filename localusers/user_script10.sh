360cc5287cf057e3806b47c2a3153299ceccc7da
Lingxue Li <ll2871@columbia.edu>
2020-12-11 06:38:53 -0700

    shell scripting: logger, return command

--- localusers/user_script10.sh
+++ localusers/user_script10.sh
@@ -5,10 +5,32 @@
 # global variable inside a function are not visible to the outside until the function is executed
 
 log() {
+	# this function sends a msg to syslog and to standard output if VERBOSE is true.
 	local MESSAGE="${@}"
 	if [[ "${VERBOSE}" = 'true' ]]
 	then
 		echo "${MESSAGE}"
+	fi
+	logger -t user-script10.sh "${MESSAGE}"
+	# sudo tail /var/log/messages
+}
+
+backup_file(){
+	# this function creates a backup of a file. Returns non-zero status on error.
+	local FILE="${1}"
+
+	# make sure the file exists
+	if [[ -f "${FILE}" ]]
+	then
+		local BACKUP_FILE="/var/tmp/$(basename ${FILE})"."$(date +%F%N)"
+		log "Backing up ${FILE} to ${BACKUP_FILE}."
+
+		# the exit status of the function will be the exit status of the cp command.
+		# -p: ownership,timestamps
+		cp -p ${FILE} ${BACKUP_FILE}
+	else
+		# the file does not exist, so return a non-zero exit status
+		return 1
 	fi
 }
 
@@ -20,4 +42,14 @@ readonly VERBOSE='true'
 log "${VERBOSITY}" 'Hello!'
 log "${VERBOSITY}" 'This is fun!'
 
+backup_file '/etc/passwd'
+
+# make a decision based on the exit status of the function
+if [[ "${?}" -eq '0' ]]
+then
+	log 'File backup succeed!'
+else
+	log 'File backup failed!'
+	exit 1
+fi
 
