bc976e999ceb69bc92490046aae3637327aa7b2e
Lingxue Li <ll2871@columbia.edu>
2020-12-16 22:54:00 -0700

    shell scriptin: pseudocode for script that scans the log file

---
+++ localusers/show-attackers.sh
@@ -0,0 +1,6 @@
+# Make sure a file was supplied as an argument.
+# Display the CSV header.
+# Loop through the list of failed attempts and corresponding IP
+addresses.
+# If the number of failed attempts is greater than the limit, display
+count, IP, and location.
