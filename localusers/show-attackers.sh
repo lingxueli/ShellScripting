9f8b07bef53a5a71ae68b037bf8aab54c2eb55f1
Lingxue Li <ll2871@columbia.edu>
2020-12-17 00:08:02 -0700

    shell scripting: awk command parsing log file

--- localusers/show-attackers.sh
+++ localusers/show-attackers.sh
@@ -1,6 +1,25 @@
+#!/bin/bash
+
+LIMIT=10
+
 # Make sure a file was supplied as an argument.
+LOG_FILE="${1}"
+if [[ ! -e "${LOG_FILE}" ]]
+then
+	echo "Cannot open log file: ${LOG_FILE}" >&2
+	exit 1
+fi
+
 # Display the CSV header.
-# Loop through the list of failed attempts and corresponding IP
-addresses.
-# If the number of failed attempts is greater than the limit, display
-count, IP, and location.
+echo "Count,IP,Location"
+
+# Loop through the list of failed attempts and corresponding IP addresses.
+# If the number of failed attempts is greater than the limit, display count, IP, and location.
+grep Failed ${LOG_FILE} | awk '{print $((NF-3))}' | sort | uniq -c | sort -nr | while read COUNT IP
+do
+	if [[ ${COUNT} -gt ${LIMIT} ]]
+	then
+		LOCATION=$(geoiplookup ${IP} | awk -F ',' '{ print $2 }')
+		echo "${COUNT}, ${IP}, ${LOCATION}"
+	fi
+done
